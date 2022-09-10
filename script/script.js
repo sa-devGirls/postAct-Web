854be61033d4df9ff231161a579aa815d2686ac3
dudaborges <duda.pborges92@gmail.com>
2022-09-09 21:44:26 -0300

    updateButton

--- script/script.js
+++ script/script.js
@@ -1,3 +1,21 @@
+const url = "index.html"
+const btn = document.getElementById('btn')
+const btn2 = document.getElementById('btn2')
+
+function openPag(url) {
+    const novaJanela = window.open(url, '_self') //ou _blank para abrir em outra janela
+    novaJanela.focus()
+}
+
+btn.addEventListener('click', () =>{
+    openPag(url)
+})
+
+btn2.addEventListener('click', () =>{
+    openPag(url)
+})
+
+
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
@@ -9,3 +27,4 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
