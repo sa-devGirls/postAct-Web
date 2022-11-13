f07507eb3ae35f77ff9239017d458304c8ea4d68
dudaborges <duda.pborges92@gmail.com>
2022-11-13 13:24:47 -0300

    added: registration field validation

--- script/script-log.js
+++ script/script-log.js
@@ -1,70 +1,78 @@
-const url = "index.html"
-const btn = document.getElementById('btn')
-const btn2 = document.getElementById('btn2')
+// const url = "index.html"
+// const btn2 = document.getElementById('btn2')
 
-function openPag(url) {
-    const novaJanela = window.open(url, '_self') //ou _blank para abrir em outra janela
-    novaJanela.focus()
-}
+// function openPag(url) {
+//     const novaJanela = window.open(url, '_self') //ou _blank para abrir em outra janela
+//     novaJanela.focus()
+// }
 
-btn.addEventListener('click', () =>{
-    openPag(url)
-})
+// btn.addEventListener('click', () =>{
+//     openPag(url)
+// })
 
-btn2.addEventListener('click', () =>{
-    openPag(url)
-})
+// btn2.addEventListener('click', () =>{
+//     openPag(url)
+// })
 
 
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const signUpButton = document.getElementById('signUp')
+const signInButton = document.getElementById('signIn')
+const container = document.getElementById('container')
 
 signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+	container.classList.add("right-panel-active")
 });
 
 signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+	container.classList.remove("right-panel-active")
 });
 
 
-//validação de campos
-function validateFields(){
-    toogleButtonsDisable()
-    toogleEmailErrors()
+
+const form = document.getElementById('form')
+const campos = document.querySelectorAll('.required')
+const spans = document.querySelectorAll('.span-required')
+const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', (event) =>{
+    event.preventDefault()
+    nameValidate()
+    emailValidate()
+    mainPasswordValidate()
+})
+
+function setEror(login_cadastro){
+    campos[login_cadastro].style.border = '2px solid #e63636'
+    spans[login_cadastro].style.display = 'block'
 }
 
-function isEmailValid(){
-    const email = document.getElementById('email').value
-    if (!email){
-        return false
+function removeError(login_cadastro){
+    campos[login_cadastro].style.border = ''
+    spans[login_cadastro].style.display = 'none'
+}
+
+function nameValidate(){
+    if(campos[0].value.length < 3){
+        setEror(0)
+    }else{
+        removeError(0)
     }
-    return validateEmail(email)
 }
 
-function toogleEmailErrors(){
-    const email = document.getElementById('email').value
-    if(!email){
-        document.getElementById('email-required-error').style.display = 'block'
+function emailValidate(){
+        if(!emailRegex.test(campos[1].value)){
+            setEror(1)
+        }else{
+            removeError(1)
+        }
+}
+
+function mainPasswordValidate(){
+    if(campos[2].value.length < 8){
+        setEror(2)
+    }else{
+        removeError(2)
     }
-}
-
-function toogleButtonsDisable(){
-    const emailValid = isEmailValid()
-    const passwordValid = isPasswordValid()
-    document.getElementById('btn2').disabled = !emailValid || !passwordValid
-}
-
-function isPasswordValid(){
-    const password = document.getElementById('password').value
-    if (!password){
-        return false
-    } 
-    return true
-    
-}
-
-function validateEmail(email){
-    return /\S+@\S+\.\S+/.test(email)
 }
