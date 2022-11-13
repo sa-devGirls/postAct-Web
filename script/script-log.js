dc70c6e1ce5e07fcb89c5d3ebfbd7beaee4aa9b2
dudaborges <duda.pborges92@gmail.com>
2022-11-13 16:21:19 -0300

    update: registration field validation

--- script/script-log.js
+++ script/script-log.js
@@ -1,20 +1,3 @@
-// const url = "index.html"
-// const btn2 = document.getElementById('btn2')
-
-// function openPag(url) {
-//     const novaJanela = window.open(url, '_self') //ou _blank para abrir em outra janela
-//     novaJanela.focus()
-// }
-
-// btn.addEventListener('click', () =>{
-//     openPag(url)
-// })
-
-// btn2.addEventListener('click', () =>{
-//     openPag(url)
-// })
-
-
 const signUpButton = document.getElementById('signUp')
 const signInButton = document.getElementById('signIn')
 const container = document.getElementById('container')
@@ -34,13 +17,33 @@ const campos = document.querySelectorAll('.required')
 const spans = document.querySelectorAll('.span-required')
 const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 
-const btn = document.getElementById('btn')
+
+// const url = "index.html"
+// const btn = document.getElementById('btn')
+
+// function openPag(url) {
+//     const novaJanela = window.open(url, '_self') //ou _blank para abrir em outra janela
+//     novaJanela.focus()
+// }
+
+// btn.addEventListener('click', () =>{
+//     openPag(url)
+// })
+
+
+function validateFields(){
+    const emailValid = isEmailValid()
+    document.getElementById('btn').disabled = !emailValid
+}
+
 
 btn.addEventListener('click', (event) =>{
     event.preventDefault()
     nameValidate()
     emailValidate()
     mainPasswordValidate()
+
+
 })
 
 function setEror(login_cadastro){
@@ -62,11 +65,18 @@ function nameValidate(){
 }
 
 function emailValidate(){
-        if(!emailRegex.test(campos[1].value)){
-            setEror(1)
-        }else{
-            removeError(1)
-        }
+    const email = document.getElementById('email').value
+
+    if(!email){
+        console.log('campo vazio')
+        return true
+    }else if(!emailRegex.test(campos[1].value)){
+        setEror(1)
+        return true  
+    }else{
+        removeError(1)
+        return false 
+    }
 }
 
 function mainPasswordValidate(){
@@ -76,3 +86,13 @@ function mainPasswordValidate(){
         removeError(2)
     }
 }
+
+
+// function register(){
+//     window.location.href = "index.html"
+// }
+
+
+// function login(){
+//     window.location.href = "index.html"
+// }
